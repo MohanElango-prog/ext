7b72fd426ff8d4d4c99bad9e132fa633b4068186
deku <deku@pop-os.localdomain>
2023-10-15 12:48:55 +0530

    send job content between content and popup scripts

--- background/background.js
+++ background/background.js
@@ -1,3 +1,10 @@
+// MESSAGES passing between content script, popup and background scripts
+const MESSAGES = {
+    GET_CONTENT: 'GET_CONTENT',
+    SEND_CONTENT: 'SEND_CONTENT'
+}
+
+
 chrome.runtime.onMessage.addListener((msg) => {
     console.log(msg)
 }) 
--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,10 @@
+// MESSAGES passing between content script, popup and background scripts
+const MESSAGES = {
+    GET_CONTENT: 'GET_CONTENT',
+    SEND_CONTENT: 'SEND_CONTENT'
+}
+
+
 const $ = s => document.querySelector(s);
 const $$ = s => document.querySelectorAll(s);
 
@@ -7,29 +14,34 @@ const jobs = {}
 const btn = $('button');
 btn.addEventListener('click', async () => {
     const [tab] = await chrome.tabs.query({ active: true })
-    
-    const {url, id} = tab
-    
-    const splitted_url = url.split('?')[0]
-    
 
-    if(splitted_url === 'https://www.linkedin.com/jobs/collections/recommended/'){
+    const { url, id } = tab
+
+    if (url.includes('https://www.linkedin.com/jobs/collections')) {
         const job_id = url.split('=').at(-1)
         jobs[job_id] = {
             url,
             id,
             done: false
         }
-        chrome.tabs.sendMessage(id, { url, id, job_id })
-        
+        chrome.tabs.sendMessage(id, { type: MESSAGES.GET_CONTENT})
+
+        console.log("request send")
     }
 
 })
 
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    const {id, content} = msg
-    console.log(content)
-    $('#content').textContent = content
+    const { id, content, control_id, type } = msg
+    
+    switch(type) {
+        case MESSAGES.SEND_CONTENT: {
+            $('#content').textContent = content
+            break
+        }
+        default:
+            break
+    }
 });
 
